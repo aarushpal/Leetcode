abe384ddaf1335316510bf8c88606551dd675541
Aarush Pal <74675248+aarushpal@users.noreply.github.com>
2023-09-03 19:44:20 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (14.84%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    // void pow(int n, double ans)
+    // {
+    //     if(n<=0) return;
+    // }
+    double myPow(double x, int n) {
+        double ans = 0;
+        ans = pow(x,n);
+        return ans;
+    }
+};
